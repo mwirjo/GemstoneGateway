db99357672e2b4812fd825fc5582f78cb464a3db
mwirjo <mwirjosentono@byupathway.edu>
2025-12-10 20:50:12 -0300

    vite env

---
+++ src/js/externalServices.js
@@ -0,0 +1,40 @@
+const baseURL = import.meta.env.VITE_SERVER_URL
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+async function convertToJson(res) {
+  const data = await res.json();
+  if (res.ok) {
+    return data;
+  } else {
+    throw { name: "servicesError", message: data };
+  }
+}
+
+export default class ExternalServices {
+  constructor() {
+    
+  } // removed ../public because the vite(?) server was complaining and I couldn't add to cart
+  async getData(category) {
+  const response = await fetch(`${baseURL}products/search/${category} `);
+  const data = await convertToJson(response);
+  return data.Result;
+}
+  async findProductById(id) {
+    const response = await fetch(`${baseURL}product/${id}`);
+    const data = await convertToJson(response);
+
+    return data.Result;
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
+  }
+
+}
