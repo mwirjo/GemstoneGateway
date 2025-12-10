a7ad77a0421870c979d1ca9d6ab0ea2d09050b08
mwirjo <mwirjosentono@byupathway.edu>
2025-12-10 19:59:34 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -9,21 +9,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(
-          __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
-        ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(
-          __dirname,
-          "src/product_pages/northface-alpine-3.html",
-        ),
-        product4: resolve(
-          __dirname,
-          "src/product_pages/northface-talus-4.html",
-        ),
+        dragon: resolve(__dirname, "src/Dragon/index.html"),
+        formula: resolve(__dirname, "src/FormulaMode/index.html"),
+        guess: resolve(__dirname,"src/FormulaMode/index.html",),
+        learn: resolve(__dirname, "src/LearnMode/index.html"),
       },
     },
   },
